76e948fc416232fd4d36b03688f83ecbfe315034
heidy <heidy.titirico.et7@gmail.com>
2024-08-23 15:34:55 -0300

    h

--- Calculadora/test/ar/edu/et7/business/CalculosTest.java
+++ Calculadora/test/ar/edu/et7/business/CalculosTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class CalculosTest {
-
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
-
+	 class calculostest {
+		 Calculos calculos;
+		 
+		 
+	 }
+	
+	@Test
+	  
+      
 }
